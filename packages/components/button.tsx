f5706479a1fa860198bcf527caaf91a4a68ba1e9
David Bleeker <dave@cataluma.com>
2024-03-19 08:58:36 -0700

    Update dependencies and styles in UI package, and add forwardRef to Button component

--- packages/components/button.tsx
+++ packages/components/button.tsx
@@ -1,16 +1,28 @@
 import clsx from 'clsx';
+import { ButtonHTMLAttributes, forwardRef } from 'react';
 import { tw } from 'utils';
 
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
 const styles = {
-  base: tw`bg-blue-500 px-4 py-2`,
+  base: tw`bg-green-500 px-4 py-2`,
 };
 
-export const Button = (props: any) => {
-  return (
-    <button className={styles.base} onClick={() => props.onClick?.()}>
-      {props.children}
-    </button>
-  );
-};
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    const { className, onClick, ...rest } = props;
+
+    return (
+      <button
+        className={clsx(styles.base, className)}
+        onClick={(e) => props.onClick?.(e)}
+        ref={ref}
+        {...rest}
+      >
+        {props.children}
+      </button>
+    );
+  },
+);
 
 export default Button;
